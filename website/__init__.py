8c807e0c09b38746a957067b2e4bf371decb9b36
Spider_999_ <harypatrascu.1@gmail.com>
2024-11-08 15:21:09 +0200

    Added login and register functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,48 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 
+
+    # Blueprint setup
     from .auth import auth
+    from .pages import pages
     app.register_blueprint(auth, url_prefix='/')
-
+    app.register_blueprint(pages, url_prefix='/')
     
+    # Database setup
+    from .models import User
+    db.init_app(app)
+    migrate.init_app(app, db)
+    create_db(app)
+
+    # Login manager setup
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app=app)
+    login_manager.login_message = "Inregistreaza-te sau autentifica-te pentru a vizualiza site-ul"
+
+    # Tell flask how to load a user, what user to look for
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+
+def create_db(app):
+    if not os.path.exists('website/db.sqlite3'):
+        with app.app_context():
+            db.create_all()
--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_user, logout_user, login_required
 from .forms import RegisterForm, LoginForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from .models import User
 
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
+        print('hello')
         password_hash = generate_password_hash(form.password.data, method='scrypt')
+        user = User(username=form.username.data, email=form.email.data, password=password_hash, user_role=form.user_role.data)
+        db.session.add(user)
+        db.session.commit()
 
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html', title='Inregistrare', form=form)
 
 
@@ -20,6 +29,18 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+
+            return redirect(url_for('pages.home'))
     
     return render_template('auth/login.html', title='Autentificare', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('pages.index'))
